41f392ba17e55268ab631b81c17ed0cabeb1d4bd
swathy s <swathysubu11@gmail.com>
2022-07-27 06:05:09 +0530

    collector frontend pages added

---
+++ frontend/cAcnt.js
@@ -0,0 +1,10 @@
+let create_shop_button=document.getElementById('create-shop-button');
+let shop_info=document.getElementById('shop-info-id');
+let first_shop_info=document.getElementById('new-shop-info');
+
+
+create_shop_button.addEventListener('click',()=>{
+    console.log('Button clicked')
+    //shop_info.style.display="none"
+
+})
